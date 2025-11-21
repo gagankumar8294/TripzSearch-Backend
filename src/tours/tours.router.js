9523805768e94122281bb5397010d75b99e980c7
gagankumar8294 <gagankumar8294@gmail.com>
2025-11-21 11:34:06 +0530

    protected routes

--- src/tours/tours.router.js
+++ src/tours/tours.router.js
@@ -12,15 +12,15 @@ const toursController = new ToursController();
 toursRouter.get("/", (req, res) => toursController.getAll(req, res));
 
 // GET single tour
-toursRouter.get("/:id", (req, res) => toursController.getById(req, res));
+toursRouter.get("/:id",  (req, res) => toursController.getById(req, res));
 
 // CREATE tour
-toursRouter.post("/",   (req, res) => toursController.add(req, res));
+toursRouter.post("/", jwtAuth,  (req, res) => toursController.add(req, res));
 
 // UPDATE tour
-toursRouter.put("/:id",  (req, res) => toursController.update(req, res));
+toursRouter.put("/:id", jwtAuth, (req, res) => toursController.update(req, res));
 
 // DELETE tour
-toursRouter.delete("/:id",  (req, res) => toursController.delete(req, res));
+toursRouter.delete("/:id", jwtAuth, (req, res) => toursController.delete(req, res));
 
 export default toursRouter;
